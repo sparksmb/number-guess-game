28c0c94b49611de8ac27bd2d03ce0b8e06274c6a
Michael Sparks <sparksmb@gmail.com>
2015-05-05 08:01:30 -0500

    added tests for main

--- 3.0/game_app/test2.rb
+++ 3.0/game_app/test2.rb
@@ -5,21 +5,6 @@ require 'entities/game_stats'
 require 'compute_game_result'
 require 'record_game_result'
 
-#g = Game.new
-#puts "guess 3, value #{g.value}"
-
-#cgr = ComputeGameResult.new(3, g)
-#gc = cgr.execute
-
-#gs = GameStats.new
-#gs.add_game(gc)
-
-#puts "length: #{gs.games.length}"
-
-#g0 = gs.games[0]
-#puts "winner?: #{ g0.winner?}"
-
-
 gs = GameStats.new
 gs.add_game(Game.new(1))
 gs.add_game(Game.new(2))
